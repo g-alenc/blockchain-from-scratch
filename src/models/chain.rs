e18105c231c6a434fc4275cad662c6a4b9c7eb2b
G-alenc <ufmgalencastro@gmail.com>
2025-11-21 14:12:23 -0300

    feat(blockchain): create the add_block method

--- src/models/chain.rs
+++ src/models/chain.rs
@@ -1,5 +1,5 @@
 use crate::models::block::Block;
-
+//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
 struct blockchain {
     chain: Vec<Block>,
     difficulty: usize,
@@ -18,7 +18,16 @@ impl blockchain{
     }
 
     pub fn add_block(&mut self, block_data: String){
+        // Get the last block hash
+        let last_block = self.chain.last().expect("The chain does not have a genesis block");
+        let last_hash = last_block.get_hash().clone();
+        
+        // Create and mine the new block
+        let mut new_block = Block::new(block_data.to_string(), last_hash);
+        new_block.mine(self.difficulty);
 
+        // Add the new block to the chain
+        self.chain.push(new_block);
     }
 }
 
